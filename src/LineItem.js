f806468b4dd3500a798c8a6390014d868fea6bd4
Amir <amirmohammedihaam@gmail.com>
2025-02-17 22:47:25 +0100

    fix bugs

--- src/LineItem.js
+++ src/LineItem.js
@@ -1,7 +1,7 @@
 import { FaTrashAlt } from "react-icons/fa";
 const LineItem = ({ item, handleChange , handleDelete }) => {
     return (
-        <li className="item" key={item.id}>
+        <li className="item">
             <input type="checkbox" onChange={() => handleChange(item.id)} checked={item.checked} />
             <label onDoubleClick={() => handleChange(item.id)} style={(item.checked) ? { textDecoration: 'line-through' } : null}>{item.item}</label>
             <FaTrashAlt onClick={() => handleDelete(item.id)} role="button" tabIndex="0" aria-label={`Delete ${item.item}`} />
