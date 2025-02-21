6478fe621e4fe70829b7fbd5aa650e02d37b2ddb
Amir <amirmohammedihaam@gmail.com>
2025-02-21 22:29:47 +0100

    complete fetch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [items, setItems] = useState([]);
   const [newItem , setNewItem] = useState('');
   const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading , setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -19,18 +21,23 @@ function App() {
         }
         const data = await response.json();
         setItems(data);
+        setFetchError(null);
       }
       catch(error){
-        console.log(error.stack);
+        setFetchError(error.message);
       }
-    }
-    (async () => {
-      await fetchItems();
-    })();
+      finally{
+        setIsLoading(false);
+    }}
+    setTimeout(() => {(async () => await fetchItems())()}, 2000);
   }, []);
   const addItem = (item) => {
+    const inputValue = item.trim();
+      if (inputValue === "") {
+        return;
+    }
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id, checked: false, item };
+    const myNewItem = { id, checked: false, inputValue };
     const newList = [...items, myNewItem];
     setItems(newList);
   }
@@ -51,14 +58,15 @@ function App() {
   }
   return (
     <div className="App">
-
       <Header />
       <SearchItem search={search} setSearch={setSearch} />
       <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
-      <Content items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))} handleChange={handleChange} handleDelete={handleDelete} />
+      <main>
+        {isLoading && <p style={{ color: 'blue', textAlign:'center' }}>Loading...</p>}
+        {fetchError && <p style={{ color: 'red', textAlign:'center' }}>{`Error : ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))} handleChange={handleChange} handleDelete={handleDelete} />}
+      </main>
       <Footer length={items.length} />
-
-
     </div>
   );
 }
